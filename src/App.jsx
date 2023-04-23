bac95f18086b4269aacde1306763f8e7a3ceca42
Lakshmikanta <frontendfixer@github.com>
2023-04-23 20:17:17 +0530

    recreate landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,23 @@
-import { useState } from 'react';
+import './App.scss';
 import { ReactComponent as ReactLogo } from './assets/react.svg';
 import { ReactComponent as ViteLogo } from './assets/vite.svg';
-import { ReactComponent as CrwnLoogo } from './assets/crown.svg';
-import './App.scss';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div>
-        <div>
-          <CrwnLoogo className='logo' />
-        </div>
-        <a href='https://vitejs.dev' target='_blank' rel='noreferrer noopener'>
-          <ViteLogo className='logo' alt='Vite logo' />
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer noopener">
+          <ViteLogo className="logo" alt="Vite logo" />
         </a>
-        <a href='https://react.dev' target='_blank' rel='noreferrer noopener'>
-          <ReactLogo className='logo react' alt='React logo' />
+        <a href="https://react.dev" target="_blank" rel="noreferrer noopener">
+          <ReactLogo className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite React Boilerplate</h1>
-      <div className='card'>
-        <button onClick={() => setCount(count => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
+      <span>
+        ⚙️ Created with [ <code>🔱 ESLint</code> | <code>🎀 Prettier</code> | <code>🎨 SCSS</code> |{' '}
+        <code>🔮 StyleLint</code> ]
+      </span>
     </>
   );
 }
