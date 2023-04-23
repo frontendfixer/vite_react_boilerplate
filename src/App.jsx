bf33d607ae49695029e645a9a7531c7555b7a7fa
Lakshmikanta <frontendfixer@github.com>
2023-04-23 23:24:15 +0530

    tailwind landing page redesign

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,29 @@
-import './App.scss';
 import { ReactComponent as ReactLogo } from './assets/react.svg';
 import { ReactComponent as ViteLogo } from './assets/vite.svg';
 
 function App() {
   return (
-    <>
-      <div>
+    <div className="flex h-screen w-screen flex-col items-center justify-center bg-black text-center text-white">
+      <div className="mb-8 flex gap-4">
         <a href="https://vitejs.dev" target="_blank" rel="noreferrer noopener">
-          <ViteLogo className="logo" alt="Vite logo" />
+          <ViteLogo className=" h-24 w-24  transition-all md:h-32 md:w-32" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank" rel="noreferrer noopener">
-          <ReactLogo className="logo react" alt="React logo" />
+          <ReactLogo
+            className="h-24 w-24 animate-spin-slow transition-all hover:animate-none md:h-32 md:w-32"
+            alt="React logo"
+          />
         </a>
       </div>
-      <h1>Vite React Boilerplate</h1>
-      <span>
-        ⚙️ Created with [ <code>🔱 ESLint</code> | <code>🎀 Prettier</code> | <code>🎨 SCSS</code> |{' '}
-        <code>🔮 StyleLint</code> ]
-      </span>
-    </>
+      <h1 className="mb-4 text-4xl font-bold transition md:text-6xl">Vite React Boilerplate</h1>
+      <p className="text-sm  md:text-xl">
+        ⚙️ Created with <br />
+        <code className=" font-mono text-neutral-70">🔱 ESLint</code> |{' '}
+        <code className=" font-mono text-neutral-70">🎀 Prettier</code> |{' '}
+        <code className=" font-mono text-neutral-70">🎨 TailwindCSS</code> |{' '}
+        <code className=" font-mono text-neutral-70">🔮 Postcss</code>
+      </p>
+    </div>
   );
 }
 
