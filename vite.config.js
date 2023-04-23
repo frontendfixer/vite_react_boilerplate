8517c40d458da5f7dbfeabfec2e657f96c0ef910
Lakshmikanta <frontendfixer@github.com>
2023-04-23 20:15:00 +0530

    switch from SWC to babel

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,20 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import react from '@vitejs/plugin-react';
 import eslintPlugin from '@nabla/vite-plugin-eslint';
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      plugins: [['@swc/plugin-styled-components', {}]],
-    }),
-    eslintPlugin(),
-    ViteImageOptimizer(),
-    svgr(),
-  ],
+  plugins: [react(), eslintPlugin(), ViteImageOptimizer(), svgr()],
   server: {
     port: 5555,
     strictPort: true,
     open: true,
   },
   build: {
+    cssCodeSplit: true,
     cssMinify: true,
     sourcemap: true,
   },
