2b292702ba36d247982d5927bc0b7f28c302a4b4
Lakshmikanta <frontendfixer@github.com>
2023-04-25 20:46:03 +0530

    ESLint and Stylelint some package changes

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import eslintPlugin from '@nabla/vite-plugin-eslint';
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslintPlugin(), ViteImageOptimizer(), svgr()],
+  plugins: [react(), ViteImageOptimizer(), svgr()],
   server: {
     port: 5555,
     strictPort: true,
